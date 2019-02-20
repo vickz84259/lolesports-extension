0d5839512360aebe1f29b6baddc37c642cd0feae
Victor Otieno Omondi <vickz84259@gmail.com>
2019-02-20 22:57:49 +0300

    Add basic sidebar
    The side bar currently only shows the team names and their respective
    logos
    
    Signed-off-by: Victor Otieno Omondi <vickz84259@gmail.com>

--- src/stats.js
+++ src/stats.js
@@ -34,9 +34,6 @@ async function getElement(id = null, selector = null) {
 }
 
 async function init() {
-  let id = document.location.href.match(/\/vod\/(\d+)\//)[1];
-  let storage = browser.storage.local.get(id);
-
   let placeholder = getElementById('video-player-placeholder');
   let videoPlayer = getElementById('video-player');
 
@@ -73,6 +70,7 @@ async function init() {
         </div>
       </div>
     </div>`;
+  addStats();
 
   placeholder = await getElementById('video-player-placeholder');
   placeholder.setAttribute('uuid', uuid);
@@ -85,6 +83,73 @@ async function init() {
     await getElementBySelector('.Footer'));
 
   (await getElementBySelector('.WatchVod')).appendChild(await nav);
+}
+
+async function addStats() {
+  let sideBar = await getElementBySelector('.tse-content .ember-view');
+  sideBar.innerHTML = `
+    <div id="stream-side-nav-container" class="side-panel-container">
+      <div class="tabs-container">
+        <div class="tabs-wrapper">
+          <ul class="tabs" data-tab="">
+              <li data-name="stats" class="side-nav-tab stats-tab larger-tab
+                survey-fire active" data-survey-key="stats-tab">
+                  <a href="#panel2-1" style="display: block;"
+                    aria-selected="true" tabindex="0">Stats</a>
+              </li>
+          </ul>
+      </div>
+      </div>
+      <div class="tabs-content-container">
+        <div class="tabs-content no-bottom">
+          <div class="content livestats-container active" id="panel2-1"
+              aria-hidden="false">
+            <div class="teams-container">
+              <div id="ember1176" class="ember-view tse-scrollable">
+                <div class="tse-scrollbar" style="display: block;">
+                  <div class="drag-handle" style="top: 2px; height: 354px;">
+                  </div>
+                </div>
+                <div class="tse-scroll-content" style="width:391px;
+                    height: 539px;">
+                  <div class="tse-content">
+                    <div id="ember1190" class="ember-view team-matchup">
+                      <div class="team-matchup-teams-section">
+                        <div class="team-matchup-blue-team">
+                        </div>
+                        <div class="team-matchup-red-team">
+                        </div>
+                        <span class="clearfix"></span>
+                      </div>
+                    </div>
+                    <div class="teams">
+                    </div>
+                    <div id="ember1183" class="ember-view">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    `;
+  let id = document.location.href.match(/\/vod\/(\d+)\//)[1];
+  let teams = (await browser.storage.local.get(id))[id].data.event.match.teams;
+
+  let selectors = ['.team-matchup-blue-team', '.team-matchup-red-team']
+  selectors.forEach(async (item, index) => {
+    (await getElementBySelector(item)).innerHTML = `
+      <div class="team-logo" style="background-size: contain; background-repeat:
+        no-repeat; background-image: url(https://am-a.akamaihd.net/image/?f=${teams[index].image}&amp;resize=55:55);">
+      </div>
+      <div class="acronym-record">
+        <div class="team-acronym">
+          ${teams[index].code}
+        </div>
+      </div>`;
+  });
 }
 
 init();
