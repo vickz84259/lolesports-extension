648e10285a7a097b8b1e60dc0ca7c5f1a2e2a624
Victor Otieno Omondi <vickz84259@gmail.com>
2019-03-06 16:49:13 +0300

    Finalise switch to templates
    The project now completely uses templates instead of template strings.
    This will make working on the html code much easier.
    
    Signed-off-by: Victor Otieno Omondi <vickz84259@gmail.com>

--- src/stats.js
+++ src/stats.js
@@ -34,35 +34,20 @@ async function getElement(id = null, selector = null) {
 }
 
 async function init() {
+  // Getting the id from the url
+  let id = document.location.href.match(/\/vod\/(\d+)\//)[1];
+  let data = browser.storage.local.get(id);
+
   let topDiv = getElementBySelector('.WatchVod');
   let nav = getElementBySelector('.WatchVod .nav');
   let placeholder = await getElementById('video-player-placeholder');
 
-  (await topDiv).outerHTML = `
-    <div id="page-container" class="main-section right-panel-container
-        content-container">
-      <div id="ember1023" class="ember-view tse-scrollable">
-        <div class="tse-scrollbar" style="display: none;">
-          <div class="drag-handle"></div>
-        </div>
-        <div class="tse-scroll-content">
-          <div class="tse-content">
-            <div class="stream-content-container">
-              <span class="clearfix"></span>
-              <div class="stream-container">
-                <div class="VideoPlayer">
-                </div>
-              </div>
-              <div class="WatchVod">
-              </div>
-            </div>
-            <div id="ember1131" class="ember-view">
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>`;
-  addStats();
+  // This is needed since the partials are being precompiled as templates
+  Handlebars.partials = Handlebars.templates;
+
+  (await topDiv).outerHTML = Handlebars.templates['main']({
+    'teams': (await data)[id].data.event.match.teams
+  });
 
   let videoPlayer = await getElementById('video-player');
   videoPlayer.setAttribute('style', '');
@@ -74,20 +59,6 @@ async function init() {
     await getElementBySelector('.Footer'));
 
   (await getElementBySelector('.WatchVod')).appendChild(await nav);
-}
-
-async function addStats() {
-  // Getting the id from the url
-  let id = document.location.href.match(/\/vod\/(\d+)\//)[1];
-  let teams = (await browser.storage.local.get(id))[id].data.event.match.teams;
-
-  // This is needed since the partials are being precompiled as templates
-  Handlebars.partials = Handlebars.templates;
-
-  let sideBar = await getElementBySelector('.tse-content .ember-view');
-  sideBar.innerHTML = Handlebars.templates['stats']({
-    'teams': teams
-  })
 }
 
 init();
