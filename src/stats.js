7829c824211644f4a1c6505dba7be967ea068740
Victor Otieno Omondi <vickz84259@gmail.com>
2019-03-23 20:30:51 +0300

    Add logic to retrieve player stats from the api.
    The logic in stats.js retrieves all the necessary details needed for the
    player stats to be displayed on the side bar.
    
    Refactored some functions in stats.js to make their purpose clearer just
    from looking at their names.
    
    Signed-off-by: Victor Otieno Omondi <vickz84259@gmail.com>

--- src/stats.js
+++ src/stats.js
@@ -47,7 +47,7 @@ async function init() {
 
   (await topDiv).outerHTML = Handlebars.templates['main']({
     'teams': (await data)[id].data.event.match.teams,
-    'showSpinner': false
+    'showSpinner': true
   });
   addStats();
 
@@ -63,25 +63,111 @@ async function init() {
   (await getElementBySelector('.WatchVod')).appendChild(await nav);
 }
 
-async function checkMatchDetails(id, matchNumber) {
+async function getIds(id, matchNumber) {
   while (true) {
-    promiseObj = browser.storage.local.get(id);
-    let games = (await promiseObj)[id].data.event.match.games;
+    let eventDetails = await browser.storage.local.get(id);
+    let games = eventDetails[id].data.event.match.games;
 
-    if ('details' in games[matchNumber - 1]) {
-      break;
+    let index = matchNumber - 1;
+    if ('details' in games[index]) {
+      return eventDetails[id].data.event.match.games[index].details;
     }
 
     await delay(timeout = 500);
   }
 }
 
+async function getMatchDetails(ids) {
+  let baseUrl = 'https://api.lolesports.com/api/v2/highlanderMatchDetails?';
+  let tournamentId = ids['tournament_id'];
+  let matchId = ids['match_id'];
+  let url = `${baseUrl}tournamentId=${tournamentId}&matchId=${matchId}`;
+
+  let data = await (await fetch(url)).json();
+  let result = {
+    'players': data.players,
+    'gameIdMappings': data.gameIdMappings
+  }
+
+  return result;
+}
+
+function getGameHash(gameId, matchDetails) {
+  for (let mapping of matchDetails.gameIdMappings) {
+    if (mapping.id === gameId) {
+      return mapping.gameHash;
+    }
+  }
+}
+
+async function getStats(tournamentId) {
+  let baseUrl = 'https://api.lolesports.com/api/v2/tournamentPlayerStats?';
+  let url = `${baseUrl}tournamentId=${tournamentId}`;
+
+  let data = await (await fetch(url)).json();
+  return data.stats;
+}
+
+async function getPlayers(platformId, gameHash) {
+  let baseUrl = 'https://acs.leagueoflegends.com/v1/stats/game/';
+  let [realm, id] = platformId.split(':');
+  let url = `${baseUrl}${realm}/${id}?gameHash=${gameHash}`;
+
+  let data = await (await fetch(url)).json();
+  let players = data.participantIdentities.map(identity => {
+    let fullName = identity.player.summonerName;
+    let [, summonerName] = fullName.split(' ');
+    return summonerName;
+  });
+
+  return players;
+}
+
 async function addStats() {
   let regexPattern = /\/vod\/(\d+)\/(\d{1})\/?/;
   let [, id, matchNumber] = document.location.href.match(regexPattern);
 
-  let check = checkMatchDetails(id, matchNumber);
-  await check;
+  let ids = await getIds(id, matchNumber);
+  let statsPromise = getStats(ids['tournament_id']);
+  let matchDetails = await getMatchDetails(ids);
+
+  let gameHash = getGameHash(ids['game_id'], matchDetails);
+  let [stats, players] = await Promise.all([
+    statsPromise,
+    getPlayers(ids['platform_id'], gameHash)
+  ]);
+
+  stats = stats.filter(item => players.includes(item.name));
+
+  let playersData = players.map(player => {
+    let object = {
+      'name': player
+    };
+    for (let playerInfo of matchDetails.players) {
+      if (playerInfo.name === player) {
+        object.photoUrl = playerInfo.photoUrl;
+        break;
+      }
+    }
+    for (let stat of stats) {
+      if (stat.name === player) {
+        object.kda = stat.kda.toPrecision(3);
+
+        kp = stat.killParticipation * 100;
+        object.kp = kp.toPrecision(2);
+        break;
+      }
+    }
+
+    return object;
+  });
+
+  let div = await getElementById('teamStats');
+  div.innerHTML = Handlebars.templates['teamStats']({
+    'showSpinner': false,
+    'blueTeam': playersData.slice(0, 5),
+    'redTeam': playersData.slice(5)
+  });
 }
 
 init();
